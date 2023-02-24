394a0c15854b8e73bd3b8a4830c88df456dfe782
94jinwoo <zuz1003@gmail.com>
2023-02-24 15:35:13 +0900

    Design: List Card CSS 추가

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -19,7 +19,7 @@ export default function Videos() {
       {isLoding && <p>Loding...</p>}
       {error && <p>something is wrong</p>}
       {videos && (
-        <ul>
+        <ul className='grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
           {videos.map((video) => {
             return <VideoCard key={video.id} video={video}></VideoCard>;
           })}
