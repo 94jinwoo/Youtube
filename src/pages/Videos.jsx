49b81353636146f9319e6ef0f4bb5574033385a0
94jinwoo <zuz1003@gmail.com>
2023-02-24 19:41:57 +0900

    Design: 상세페이지 CSS 추가

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -11,7 +11,9 @@ export default function Videos() {
     isLoding,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], () => youtube.search(keyword));
+  } = useQuery(['videos', keyword], () => youtube.search(keyword), {
+    staleTime: 1000 * 60 * 1,
+  });
 
   return (
     <>
