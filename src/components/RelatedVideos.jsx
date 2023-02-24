d47bd2fce58e37f238ca79ff4fc9c0e7058b4524
94jinwoo <zuz1003@gmail.com>
2023-02-24 19:06:48 +0900

    Feat: 상세페이지 구현

--- src/components/RelatedVideos.jsx
+++ src/components/RelatedVideos.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import VideoCard from '../components/VideoCard';
 import { useYoutubeApi } from '../context/YoutubeApiContext';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from './VideoCard';
 
-export default function Videos() {
-  const { keyword } = useParams();
+export default function RelatedVideos({ id }) {
   const { youtube } = useYoutubeApi();
   const {
     isLoding,
     error,
     data: videos,
-  } = useQuery(['videos', keyword], () => youtube.search(keyword));
+  } = useQuery(['related', id], () => youtube.relatedVideos(id));
 
   return (
     <>
       {isLoding && <p>Loding...</p>}
       {error && <p>something is wrong</p>}
       {videos && (
-        <ul className='grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
+        <ul>
           {videos.map((video) => {
             return <VideoCard key={video.id} video={video}></VideoCard>;
           })}
